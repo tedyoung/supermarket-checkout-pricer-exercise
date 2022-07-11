c93920b2c91557567463ce3d6fcad59cc4204fbf
Ted M. Young <tedyoung@gmail.com>
2022-07-11 13:36:17 -0700

    Now using Product object internally

--- src/main/java/dev/ted/supermarket/domain/Cart.java
+++ src/main/java/dev/ted/supermarket/domain/Cart.java
@@ -7,22 +7,22 @@ import java.util.List;
 // ideas for naming:
 // Order, Bag, Scale
 public class Cart {
-    private final List<String> products = new ArrayList<>();
+    private final List<Product> products = new ArrayList<>();
     private BigDecimal total = BigDecimal.ZERO;
 
     public boolean add(Product product) {
-        BigDecimal price = discountedProductPrice(product.upc(), product.productPrice());
-        if (products.contains(product.upc())) {
+        BigDecimal price = discountedProductPrice(product);
+        if (products.contains(product)) {
             price = product.productPrice().divide(BigDecimal.valueOf(2));
         }
         total = total.add(price);
-        return products.add(product.upc());
+        return products.add(product);
     }
 
-    private BigDecimal discountedProductPrice(String upc, BigDecimal productPrice) {
-        BigDecimal actualPrice = productPrice;
-        if (upc.equals("0987")) {
-            actualPrice = productPrice.multiply(BigDecimal.valueOf(0.9));
+    private BigDecimal discountedProductPrice(Product product) {
+        BigDecimal actualPrice = product.productPrice();
+        if (product.upc().equals("0987")) {
+            actualPrice = actualPrice.multiply(BigDecimal.valueOf(0.9));
         }
         return actualPrice;
     }
@@ -36,6 +36,10 @@ public class Cart {
     }
 
     public Receipt receipt() {
-        return new Receipt(total, List.copyOf(products));
+        List<String> upcs = products
+                .stream()
+                .map(Product::upc)
+                .toList();
+        return new Receipt(total, upcs);
     }
 }
