f953a6d98b10dad21c1e7ae30b34e4f00f0b3126
Ted M. Young <tedyoung@gmail.com>
2022-07-18 17:49:44 -0700

    (behavior) POST endpoint for root now exists and redirects

--- src/main/java/dev/ted/supermarket/adapter/in/web/ProductScannerController.java
+++ src/main/java/dev/ted/supermarket/adapter/in/web/ProductScannerController.java
@@ -3,6 +3,7 @@ package dev.ted.supermarket.adapter.in.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProductScannerController {
@@ -11,6 +12,11 @@ public class ProductScannerController {
     public String scanProduct(Model model) {
         model.addAttribute("upc", "");
         return "scan";
+    }
+
+    @PostMapping("/")
+    public String addProduct() {
+        return "redirect:";
     }
 
 }
--- src/test/java/dev/ted/supermarket/adapter/in/web/ProductScannerWebMvcTest.java
+++ src/test/java/dev/ted/supermarket/adapter/in/web/ProductScannerWebMvcTest.java
@@ -7,8 +7,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @Tag("integration")
 @WebMvcTest
@@ -21,6 +21,12 @@ public class ProductScannerWebMvcTest {
     public void getRootPathIs200Ok() throws Exception {
         mockMvc.perform(get("/"))
                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void postToRootPathIsRedirect() throws Exception {
+        mockMvc.perform(post("/"))
+               .andExpect(status().is3xxRedirection());
     }
 
 }
