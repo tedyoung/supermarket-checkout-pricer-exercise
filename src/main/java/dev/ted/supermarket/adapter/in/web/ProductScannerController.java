f7fc1c99c46ab755e0b9f730d8be25f2785c5090
Ted M. Young <tedyoung@gmail.com>
2022-07-18 17:44:27 -0700

    (behavior) Scan Product page now has a form with input field for UPC

--- src/main/java/dev/ted/supermarket/adapter/in/web/ProductScannerController.java
+++ src/main/java/dev/ted/supermarket/adapter/in/web/ProductScannerController.java
@@ -1,13 +1,15 @@
 package dev.ted.supermarket.adapter.in.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductScannerController {
 
     @GetMapping("/")
-    public String scanProduct() {
+    public String scanProduct(Model model) {
+        model.addAttribute("upc", "");
         return "scan";
     }
 
--- src/test/java/dev/ted/supermarket/adapter/in/web/ProductScannerControllerTest.java
+++ src/test/java/dev/ted/supermarket/adapter/in/web/ProductScannerControllerTest.java
@@ -1,6 +1,8 @@
 package dev.ted.supermarket.adapter.in.web;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.ui.ConcurrentModel;
+import org.springframework.ui.Model;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -10,11 +12,14 @@ class ProductScannerControllerTest {
     public void scanProductReturnsScanTemplate() throws Exception {
         ProductScannerController productScannerController =
                 new ProductScannerController();
+        Model model = new ConcurrentModel();
 
-        String page = productScannerController.scanProduct();
+        String page = productScannerController.scanProduct(model);
 
         assertThat(page)
                 .isEqualTo("scan");
+        assertThat(model.containsAttribute("upc"))
+                .isTrue();
     }
 
 }
