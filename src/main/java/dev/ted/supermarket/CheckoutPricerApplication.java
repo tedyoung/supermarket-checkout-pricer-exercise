80d57c5d0cf31640e8971d91c077f018f87fe611
Ted M. Young <tedyoung@gmail.com>
2022-07-18 17:22:34 -0700

    Added Spring Boot application (bootstrap/startup) and the test. Split test configurations into Unit and Integration.

---
+++ src/main/java/dev/ted/supermarket/CheckoutPricerApplication.java
@@ -0,0 +1,13 @@
+package dev.ted.supermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutPricerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutPricerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ted/supermarket/CheckoutPricerApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.ted.supermarket;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Tag("integration")
+@SpringBootTest
+class CheckoutPricerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
